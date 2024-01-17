b84827f32a19388f3e2a48b8cb6f119179458261
lu li <lilu@idea.edu.cn>
2024-01-17 19:48:43 +0800

    processing

--- multiprocess/askgpt4.py
+++ multiprocess/askgpt4.py
@@ -17,36 +17,37 @@ def askgpt4(prompt):
     return response.choices[0].message.content
 
 
-def process_lines(start, end, lines, output_file):
+def process_lines(start, end, lines, output_file, func):
     # 仅处理分配的行
     with open(output_file, 'a', encoding='utf-8') as outfile:
         for i in range(start, min(end, len(lines))):
             json_obj = json.loads(lines[i].strip())
             try:
                 prompt = json_obj['prompt']
-                answer = askgpt4(prompt)
+                answer = func(prompt)
                 json_obj['answer'] = answer
                 json.dump(json_obj, outfile, ensure_ascii=False)
                 outfile.write("\n")
+                outfile.flush()
             except Exception as e:
                 print("error prompt:\n",json_obj['prompt'])
                 print(f"Error processing line: {e}")
     print(f'{output_file} finished.')
 
 
-def read_and_ask_parallel(file_path, chunk_size = 10):
+def read_and_ask_parallel(file_path, output_dir="results", chunk_size=10, func=askgpt4):
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     
     # 每个进程处理的行数
     processes = []
     for i in range(0, len(lines), chunk_size):
-        output_file = f'results/output_process_{i//chunk_size}.json'
+        output_file = f'{output_dir}/output_process_{i//chunk_size}.json'
         # 检查文件是否存在，如果存在，则删除
         if os.path.exists(output_file):
             os.remove(output_file)
         
-        process = multiprocessing.Process(target=process_lines, args=(i, i+chunk_size, lines, output_file))
+        process = multiprocessing.Process(target=process_lines, args=(i, i+chunk_size, lines, output_file, func))
         processes.append(process)
         process.start()
     
